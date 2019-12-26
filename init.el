d251a46796fa5ccd94e78cc9ef52688aa412d2fb
Arnau Abella <arnauabella@gmail.com>
2019-12-26 10:48:42 +0100

    migrate from macos to linux

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
 (setq 
   package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                      ("org" . "http://orgmode.org/elpa/")
-                     ("melpa" . "http://melpa.org/packages/")
+                     ("melpa" . "https://melpa.org/packages/")
                      ("melpa-stable" . "http://stable.melpa.org/packages/"))
   package-archive-priorities '(("melpa" . 1)))
 
@@ -63,15 +63,77 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#073642" "#dc322f" "#859900" "#b58900" "#268bd2" "#d33682" "#2aa198" "#657b83"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#2aa198")
+ '(cua-normal-cursor-color "#839496")
+ '(cua-overwrite-cursor-color "#b58900")
+ '(cua-read-only-cursor-color "#859900")
  '(custom-enabled-themes (quote (planet)))
  '(custom-safe-themes
    (quote
-    ("8ffdc8c66ceeaf7921f4510a70d808f01b303e6b4d177c947b442e80d4228678" "9f65322594fcff32425175b3c141ccd29f0cfb24d87b2ddea34f5d12499bfe31" "5c9bd73de767fa0d0ea71ee2f3ca6fe77261d931c3d4f7cca0734e2a3282f439" default)))
+    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8ffdc8c66ceeaf7921f4510a70d808f01b303e6b4d177c947b442e80d4228678" "9f65322594fcff32425175b3c141ccd29f0cfb24d87b2ddea34f5d12499bfe31" "5c9bd73de767fa0d0ea71ee2f3ca6fe77261d931c3d4f7cca0734e2a3282f439" default)))
  '(dante-load-flags (quote ("-Wall" "-fno-warn-type-defaults" "+c")))
  '(dante-repl-command-line (quote ("ghci")))
+ '(fci-rule-color "#073642")
+ '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-foreground-color "#93a1a1")
+ '(highlight-tail-colors
+   (quote
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#7B6000" "#8B2C02" "#990A1B" "#93115C" "#3F4D91" "#00629D" "#00736F" "#546E00")))
+ '(hl-fg-colors
+   (quote
+    ("#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36" "#002b36")))
+ '(magit-diff-use-overlays nil)
  '(package-selected-packages
    (quote
-    (java-imports flycheck-cask popup-imenu lsp-scala tide lsp-ui exec-path-from-shell use-package-chords key-chord markdown-mode yaml-mode restclient smartparens undo-tree dante lsp-mode dumb-jump use-package projectile neotree magit intero highlight-symbol goto-chg dirtree all-the-icons))))
+    (company-lsp java-imports flycheck-cask popup-imenu lsp-scala tide lsp-ui exec-path-from-shell use-package-chords key-chord markdown-mode yaml-mode restclient smartparens undo-tree dante lsp-mode dumb-jump use-package projectile neotree magit intero highlight-symbol goto-chg dirtree all-the-icons)))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
+ '(term-default-bg-color "#002b36")
+ '(term-default-fg-color "#839496")
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#dc322f")
+     (40 . "#c37300")
+     (60 . "#b97d00")
+     (80 . "#b58900")
+     (100 . "#a18700")
+     (120 . "#9b8700")
+     (140 . "#948700")
+     (160 . "#8d8700")
+     (180 . "#859900")
+     (200 . "#5a942c")
+     (220 . "#439b43")
+     (240 . "#2da159")
+     (260 . "#16a870")
+     (280 . "#2aa198")
+     (300 . "#009fa7")
+     (320 . "#0097b7")
+     (340 . "#008fc7")
+     (360 . "#268bd2"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
 
 ;;; *  Miscelaneous settings 
 
@@ -144,10 +206,10 @@
 (global-set-key (kbd "C-c <right>") 'windmove-right)
 (global-set-key (kbd "C-c <up>") 'windmove-up)
 (global-set-key (kbd "C-c <down>") 'windmove-down)
-(global-set-key (kbd "M-<left>") 'shrink-window-horizontally)
-(global-set-key (kbd "M-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "M-<down>") 'enlarge-window)
-(global-set-key (kbd "M-<up>") 'shrink-window)
+(global-set-key (kbd "ESC <left>") 'shrink-window-horizontally)
+(global-set-key (kbd "ESC <right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "ESC <down>") 'enlarge-window)
+(global-set-key (kbd "ESC <up>") 'shrink-window)
 (global-set-key (kbd "S-<left>") 'backward-word)
 (global-set-key (kbd "S-<right>") 'forward-word)
 (global-set-key (kbd "C-x f") 'find-file)
@@ -163,13 +225,34 @@
 ;;; * Icons and fonts
 
 (require 'all-the-icons)
-
+ 
+;;; * Cua-mode
+(cua-mode t)
+    (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
+    (transient-mark-mode 1) ;; No region when it is not highlighted
+    (setq cua-keep-region-after-copy t) ;; Standard Windows behaviour
+    
 ;;; * NeoTree 
 
 (require 'neotree)
 (setq neo-theme (if (display-graphic-p) 'icons 'arrows)) 
 (setq projectile-switch-project-action 'neotree-projectile-action)
 (setq neo-smart-open t)
+
+;;; * Flex search
+(use-package flx-ido
+  :demand
+  :init
+  (setq
+   ido-enable-flex-matching t
+   ;; C-d to open directories
+   ;; C-f to revert to find-file
+   ido-show-dot-for-dired nil
+   ido-enable-dot-prefix t)
+  :config
+  (ido-mode 1)
+  (ido-everywhere 1)
+  (flx-ido-mode 1))
 
 ;;; * Projectile
 
@@ -236,6 +319,12 @@
   (smartparens-global-mode t)
 (show-smartparens-global-mode t))
 
+;;; * Snippets
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :commands yas-minor-mode
+  :config (yas-reload-all))
+
 ;;;  * Version control
 
 (use-package magit
@@ -277,35 +366,14 @@
          ("\\.markdown\\'" . markdown-mode))
 :init (setq markdown-command "multimarkdown"))
 
-;;; * Haskell 
-
-(use-package dante
-  :ensure t
-  :after haskell-mode
-
-  :commands 'dante-mode
-  :init
-  (add-hook 'haskell-mode-hook 'dante-mode)
-  (add-hook 'haskell-mode-hook 'flycheck-mode)
-  (add-hook 'dante-mode-hook
-    '(lambda () (flycheck-add-next-checker 'haskell-dante
-                                           '(warning . haskell-hlint))))
-  (add-hook 'haskell-mode-hook 
-  (lambda ()
-    (linum-mode 1))))
-
-(setq flymake-no-changes-timeout nil)
-(setq flymake-start-syntax-check-on-newline nil)
-(setq flycheck-check-syntax-automatically '(save mode-enabled))
-
 ;;; * Scala
 
-;; (use-package scala-mode
-;;   :ensure t)
+(use-package lsp-ui
+  :ensure t
+  :hook (lsp-mode . lsp-ui-mode))
 
 (use-package scala-mode
-  :interpreter
-  ("scala" . scala-mode))
+  :mode "\\.s\\(cala\\|bt\\)$")
 
 (use-package sbt-mode
   :commands sbt-start sbt-command
@@ -317,23 +385,24 @@
    'self-insert-command
    minibuffer-local-completion-map))
 
+;; Enable nice rendering of diagnostics like compile errors.
 (use-package flycheck
   :init (global-flycheck-mode))
-
-(use-package lsp-mode
-  :ensure t)
 
 (use-package lsp-ui
   :ensure t
   :hook (lsp-mode . lsp-ui-mode))
 
+;; Add company-lsp backend for metals
+(use-package company-lsp)
+
 (use-package lsp-scala
-  :load-path "~/Documents/lsp-scala"
   :after scala-mode
   :demand t
   ;; Optional - enable lsp-scala automatically in scala files
-  :hook (scala-mode . lsp)
-  :init (setq lsp-scala-server-command "/usr/local/bin/metals-emacs"))
+  :hook (scala-mode . lsp))
+
+;;; * Haskell
 
 ;;; * Html, CSS, JS
 
